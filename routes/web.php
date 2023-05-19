0de12bda6b2c48f2d5a8013c0346a882c1497e39
unknown <waisi.unas@gmail.com>
2023-05-19 11:55:07 +0500

    =Admin creation code

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\auth\AuthController;
-use App\Http\Controllers\CourseController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\auth\AuthController;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +20,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [AuthController::class, 'login_view'])->name('login');
 Route::post('/', [AuthController::class, 'login_process']);
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/create', function () {
+    $data = [
+        'name' => 'The Admin',
+        'email' => 'admin@gmail.com',
+        'password' => Hash::make('12345'),
+    ];
+    User::create($data);
+    return redirect()->route('login');
+});
 
 Route::get('/courses', [CourseController::class, 'index'])->name('courses');
 Route::get('/course/create', [CourseController::class, 'create'])->name('course.create');
